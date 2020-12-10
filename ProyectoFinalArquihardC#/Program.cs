9bb2f8cf37efb708d8f968de7524724b3e2b9fd3
marcopza <marcopza99@hotmail.com>
2020-12-10 18:00:21 -0500

    Implemented fully in c#

--- ProyectoFinalArquihardC#/Program.cs
+++ ProyectoFinalArquihardC#/Program.cs
@@ -11,10 +11,11 @@ namespace ProyectoFinalArquihard
     {
         static void Main(string[] args)
         {
-            Bitmap img = new Bitmap("*imagePath*");
-            Bitmap img2 = (Bitmap) img.Clone();
+            string path = @"..\..\resources\1.bmp";
+            string path2 = @"..\..\resources\resultado.bmp";
+            Bitmap img = (Bitmap)Image.FromFile(path);
             Int64 count = 0;
-            Int64 version = 1;
+            Int64 version = 4;
             switch (version)
             {
 
@@ -31,7 +32,7 @@ namespace ProyectoFinalArquihard
                             byte green = (byte)(255 - pixel.G);
                             byte blue = (byte)(255 - pixel.B);
                             Color newCo = Color.FromArgb(red, green, blue);
-                            img2.SetPixel(i, j, newCo);
+                            img.SetPixel(i, j, newCo);
                             count += 3;
                         }
                     }
@@ -48,10 +49,8 @@ namespace ProyectoFinalArquihard
                         {
                             Color pixel = img.GetPixel(i, j);
                             byte red = (byte)(255 - pixel.R);
-                            byte green = pixel.G;
-                            byte blue = pixel.B;
-                            Color newCo = Color.FromArgb(red, green, blue);
-                            img2.SetPixel(i, j, newCo);
+                            Color newCo = Color.FromArgb(red, pixel.G, pixel.B);
+                            img.SetPixel(i, j, newCo);
                             count += 1;
                         }
                     }
@@ -61,11 +60,9 @@ namespace ProyectoFinalArquihard
                         for (int j = 0; j < img.Height; j++)
                         {
                             Color pixel = img.GetPixel(i, j);
-                            byte red = img2.GetPixel(i, j).R;
                             byte green = (byte)(255 - pixel.G);
-                            byte blue = img2.GetPixel(i, j).B;
-                            Color newCo = Color.FromArgb(red, green, blue);
-                            img2.SetPixel(i, j, newCo);
+                            Color newCo = Color.FromArgb(pixel.R, green, pixel.B);
+                            img.SetPixel(i, j, newCo);
                             count += 1;
                         }
                     }
@@ -75,11 +72,9 @@ namespace ProyectoFinalArquihard
                         for (int j = 0; j < img.Height; j++)
                         {
                             Color pixel = img.GetPixel(i, j);
-                            byte red = img2.GetPixel(i, j).R;
-                            byte green = img2.GetPixel(i, j).G;
                             byte blue = (byte)(255 - pixel.B);
-                            Color newCo = Color.FromArgb(red, green, blue);
-                            img2.SetPixel(i, j, newCo);
+                            Color newCo = Color.FromArgb(pixel.R, pixel.G, blue);
+                            img.SetPixel(i, j, newCo);
                             count += 1;
                         }
                     }
@@ -98,7 +93,7 @@ namespace ProyectoFinalArquihard
                             byte green = (byte)(255 - pixel.G);
                             byte blue = (byte)(255 - pixel.B);
                             Color newCo = Color.FromArgb(red, green, blue);
-                            img2.SetPixel(i, j, newCo);
+                            img.SetPixel(i, j, newCo);
                             count += 3;
                         }
                     }
@@ -114,24 +109,21 @@ namespace ProyectoFinalArquihard
                         {
                             Color pixel = img.GetPixel(i, j);
                             byte red = (byte)(255 - pixel.R);
-                            byte green = pixel.G;
-                            byte blue = pixel.B;
-                            Color newCo = Color.FromArgb(red, green, blue);
-                            img2.SetPixel(i, j, newCo);
+                            Color newCo = Color.FromArgb(red, pixel.G, pixel.B);
+                            img.SetPixel(i, j, newCo);
                             count += 1;
                         }
                     }
 
-                    for (int i = img.Width; i >= 0; i--)
+                    for (int i = img.Width - 1; i >= 0; i--)
                     {
-                        for (int j = img.Height; j >= 0; j--)
+                        for (int j = img.Height - 1; j >= 0; j--)
                         {
                             Color pixel = img.GetPixel(i, j);
-                            byte red = img2.GetPixel(i, j).R;
                             byte green = (byte)(255 - pixel.G);
                             byte blue = (byte)(255 - pixel.B);
-                            Color newCo = Color.FromArgb(red, green, blue);
-                            img2.SetPixel(i, j, newCo);
+                            Color newCo = Color.FromArgb(pixel.R, green, blue);
+                            img.SetPixel(i, j, newCo);
                             count += 2;
                         }
                     }
@@ -141,40 +133,41 @@ namespace ProyectoFinalArquihard
                     /*
                      * Versión 5
                      */
-                    for (int i = 0; i < img.Width; i += 2)
+                    for (int i = 0; i < img.Width - 1; i += 2)
                     {
-                        for (int j = 0; j < img.Height; j += 2)
+                        for (int j = 0; j < img.Height - 1; j += 2)
                         {
                             Color pixel = img.GetPixel(i, j);
                             byte red = (byte)(255 - pixel.R);
                             byte green = (byte)(255 - pixel.G);
                             byte blue = (byte)(255 - pixel.B);
                             Color newCo = Color.FromArgb(red, green, blue);
-                            img2.SetPixel(i, j, newCo);
+                            img.SetPixel(i, j, newCo);
 
                             Color pixel2 = img.GetPixel(i + 1, j);
                             byte red2 = (byte)(255 - pixel2.R);
                             byte green2 = (byte)(255 - pixel2.G);
                             byte blue2 = (byte)(255 - pixel2.B);
                             Color newCo2 = Color.FromArgb(red2, green2, blue2);
-                            img2.SetPixel(i + 1, j, newCo2);
+                            img.SetPixel(i + 1, j, newCo2);
 
                             Color pixel3 = img.GetPixel(i, j + 1);
                             byte red3 = (byte)(255 - pixel3.R);
                             byte green3 = (byte)(255 - pixel3.G);
                             byte blue3 = (byte)(255 - pixel3.B);
                             Color newCo3 = Color.FromArgb(red3, green3, blue3);
-                            img2.SetPixel(i, j + 1, newCo3);
+                            img.SetPixel(i, j + 1, newCo3);
 
                             Color pixel4 = img.GetPixel(i + 1, j + 1);
                             byte red4 = (byte)(255 - pixel4.R);
                             byte green4 = (byte)(255 - pixel4.G);
                             byte blue4 = (byte)(255 - pixel4.B);
                             Color newCo4 = Color.FromArgb(red4, green4, blue4);
-                            img2.SetPixel(i + 1, j + 1, newCo4);
+                            img.SetPixel(i + 1, j + 1, newCo4);
 
                             count += 12;
                         }
+
                     }
                     break;
 
@@ -183,7 +176,8 @@ namespace ProyectoFinalArquihard
                     break;
 
             }
-
+            Image img1 = (Image) img.Clone();
+            img1.Save(path2);
         }
     }
 }
